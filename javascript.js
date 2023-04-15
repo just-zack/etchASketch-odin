5e8ba6bed42117bab9c2936cad2cf4c711beaa53
Zach_Huang <128434708+zach-huang@users.noreply.github.com>
2023-04-14 14:26:20 -1000

    Add JS logic that allows grid size changes between S M L

--- javascript.js
+++ javascript.js
@@ -1,11 +1,14 @@
 const gamePanel = document.querySelector('#gamePanel');
 
+
 function createGameGrid (n) {
     const gamePanel = document.querySelector('#gamePanel');
     for (let i =0; i < n; i++) {
         const gameDiv = document.createElement('div');
         gameDiv.classList.add('gameDiv');
         gamePanel.appendChild(gameDiv);
+        gameDiv.style.minWidth = (100/Math.sqrt(n)) +"%"
+        gameDiv.style.minHeight = (100/Math.sqrt(n)) +"%"
     }
 };
-createGameGrid(16);
+createGameGrid(64);
