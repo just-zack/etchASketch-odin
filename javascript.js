b3e7c86bb54b01cd53424cec14411dbb0fe5df7f
Zach_Huang <128434708+zach-huang@users.noreply.github.com>
2023-04-14 15:02:42 -1000

    Fix event listener and Large game grid from 128 to 256

--- javascript.js
+++ javascript.js
@@ -1,7 +1,22 @@
 const gamePanel = document.querySelector('#gamePanel');
+const game16 = document.querySelector('.size16');
+const game64 = document.querySelector('.size64');
+const game256 = document.querySelector('.size256');
+
+game16.addEventListener('click', () => {
+    createGameGrid(16);
+});
+
+game64.addEventListener('click', () => {
+    createGameGrid(64);
+});
+
+game256.addEventListener('click', () => {
+    createGameGrid(256);
+});
 
 function createGameGrid (n) {
-    clearGameGrid;
+    clearGameGrid();
     for (let i =0; i < n; i++) {
         const gameDiv = document.createElement('div');
         gameDiv.classList.add('gameDiv');
